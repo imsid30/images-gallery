8f975b27382e516b56c3f3a648c2f12dd1d3ee52
Siddharth Gupta <devguptasg94@gmail.com>
2023-08-13 01:14:43 +0530

    ImageCard component and set using map function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,47 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/header';
-import Search from './components/Search';
-import { useState } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/header";
+import Search from "./components/Search";
+import { useState } from "react";
+import ImageCard from "./components/ImageCards";
 
-const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY; 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
+  const [searchWord, setSearchWord] = useState("");
+  const [images, setImages] = useState([]);
 
-  const [searchWord,setSearchWord] = useState('');
-
-  
+  console.log(images);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(searchWord);
-    fetch(`https://api.unsplash.com/photos/random/?query=${searchWord}&client_id=${UNSPLASH_KEY}`)
-      .then((res)=> res.json())
-      .then((data)=>{
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${searchWord}&client_id=${UNSPLASH_KEY}`,
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages([{ ...data, title: searchWord }, ...images]);
         console.log(data);
       })
-      .catch((err)=>{
+      .catch((err) => {
         console.log(err);
-      })
-    setSearchWord('');
-  }
+      });
+    setSearchWord("");
+  };
 
   return (
     <div>
       <Header tittle="Images Gallery"></Header>
-      <Search searchWord = {searchWord} setSearchWord={setSearchWord} handleSubmit = {handleSearchSubmit}/>
+      <Search
+        searchWord={searchWord}
+        setSearchWord={setSearchWord}
+        handleSubmit={handleSearchSubmit}
+      />
+      {images.map((image, i) => (
+        <ImageCard key={i} image={image} />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,24 +1,27 @@
+/* eslint-disable prettier/prettier */
 import React from "react";
-import { Container, Row, Col,Form, Button } from "react-bootstrap";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 //import styles from './style/mainPage.module.css'
 
-const Search = ({handleSubmit,searchWord,setSearchWord}) => {
+const Search = ({ handleSubmit, searchWord, setSearchWord }) => {
   return (
     <Container className="mt-4">
       <Row className="justify-content-center">
-        <Col xs={12} md={8} lg={6} >
+        <Col xs={12} md={8} lg={6}>
           <Form onSubmit={handleSubmit}>
             <Row>
               <Col xs={9}>
-                <Form.Control 
-                type="text"
-                value={searchWord}
-                onChange={(e) => setSearchWord(e.target.value)}
-                placeholder="Search for new Image..." 
+                <Form.Control
+                  type="text"
+                  value={searchWord}
+                  onChange={(e) => setSearchWord(e.target.value)}
+                  placeholder="Search for new Image..."
                 />
               </Col>
               <Col>
-                <Button type="submit" variant="primary">Search</Button>
+                <Button type="submit" variant="primary">
+                  Search
+                </Button>
               </Col>
             </Row>
           </Form>
